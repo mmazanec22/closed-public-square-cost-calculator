710b18a1708d1f9f48673bc94f4f0f22f6fcced8
mmazanec22 <melanie.mazanec@gmail.com>
2016-10-17 16:38:12 -0500

    Improve design and add source links

--- application.js
+++ application.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-  $(".dollar-amount").hide()
-  $(".text-amount").hover(
-    function(){
-      ($(this).find($(".dollar-amount"))).show()
-    })
 
   var annualCost = 1550000
   var startDate = new Date('08 01 2016')
@@ -13,9 +8,25 @@ $(document).ready(function(){
     var now = new Date()
     var dif = now.getTime() - startDate.getTime()
     var secondsBetween = Math.abs(dif/1000)
-    var currentCost = "$" + (costPerSecond * secondsBetween).toFixed(2)
-    $(".calculator").find(".rolling-dollar-amount").text(currentCost)
+    $(".calculator").find(".rolling-amount").text(convertToCurrency(costPerSecond * secondsBetween))
   }
   setInterval(update, 1000)
 
 })
+
+var today = function(){
+
+}
+
+
+var convertToCurrency = function(dollarsAsInteger){
+  dollarsAsInteger = dollarsAsInteger.toFixed(2)
+  var wholeNum = dollarsAsInteger.split(".")[0].split("")
+  var numTimesDo = wholeNum.length
+  for(var i = 1; i < numTimesDo/3; i ++){
+    var index = wholeNum.length - (3 * i)
+    wholeNum.splice(index, 0, ",")
+  }
+  wholeNum = wholeNum.join("")
+  return "$" + wholeNum + "." +dollarsAsInteger.split(".")[1]
+}
